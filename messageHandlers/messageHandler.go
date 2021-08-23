3081ccc1a8f70c39483b853199ff0145ab054f66
emahfoud <elias.mahfoud@gmail.com>
2021-08-23 03:20:11 -0700

    send formatted message

--- messageHandlers/messageHandler.go
+++ messageHandlers/messageHandler.go
@@ -73,33 +73,6 @@ func status(botId string, chatId int64, config config.ServiceConfig) {
 	maxLength := 0
 
 	for _, service := range config.Services {
-		//statusCmd := exec.Command("systemctl", "status", service.Service)
-		//grepCmd := exec.Command("grep", "-Po", `"(?<=Active: )\w+"`)
-		////r, w := io.Pipe()
-		////statusCmd.Stdout = w
-		////grepCmd.Stdin = r
-		//statusOut, err1 := statusCmd.StdoutPipe()
-		//logError("status cmd: ", err1)
-		//grepCmd.Stdin = statusOut
-		//grepout, err2 := grepCmd.StdoutPipe()
-		//logError("grep cmd: ", err2)
-		//
-		//if err := statusCmd.Start(); err != nil {
-		//	logError("status cmd start: ", err)
-		//}
-		//if err := grepCmd.Start(); err != nil {
-		//	logError("grep cmd start: ", err)
-		//}
-		//if err := statusCmd.Wait(); err != nil {
-		//	logError("status cmd wait: ", err)
-		//}
-		//cmdResult, _ := io.ReadAll(grepout)
-		//if err := grepCmd.Wait(); err != nil {
-		//	logError("grep cmd wait: ", err)
-		//}
-		//
-		//out := string(cmdResult)
-
 		out, err := exec.Command("bash", "-c", fmt.Sprintf(`systemctl status %s | grep -Po "(?<=Active: )\w+"`, service.Service)).Output()
 		logError("bash cmd: ", err)
 
@@ -112,7 +85,11 @@ func status(botId string, chatId int64, config config.ServiceConfig) {
 	}
 
 	result := generateTable(maxLength, config, resultMap)
-	telegram.SendBotMessageSimple(result, botId, chatId)
+	telegram.SendBotMessage(telegram.SendMessageBody{
+		ChatID: chatId,
+		Text: result,
+		ParseMode: "MarkdownV2",
+	}, botId)
 }
 
 func logError(message string, err error) {
